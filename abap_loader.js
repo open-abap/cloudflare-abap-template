c5382aa2142a3bd826e259f046c03aeea1e03dca
larshp <larshp@hotmail.com>
2020-03-15 13:49:28 +0100

    update to latest transpiler

--- abap_loader.js
+++ abap_loader.js
@@ -2,7 +2,7 @@ const transpiler = require("@abaplint/transpiler");
 
 module.exports = async function(source, map, meta) {
   const t = new transpiler.Transpiler();
-  const result = await t.runMulti([{filename: this.resourcePath, contents: source}]);
+  const result = await t.run([{filename: this.resourcePath, contents: source}]);
   return "const abap = require('@abaplint/runtime');\n" +
     "export " + result.js[0].contents;
 }
